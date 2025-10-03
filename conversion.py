a0c73b29d920cfd905b4f40256b4472a6801843f
Snehamn24 <snehamn216@gmail.com>
2025-10-03 20:36:12 +0530

    Conversion from integer to decimal

---
+++ conversion.py
@@ -0,0 +1,8 @@
+#converting from the integer to the Decimal using decimal module and Decimal function inside the decimal module
+
+import decimal
+
+a = 123
+print(type(a))
+
+print(type(decimal.Decimal(a)))
